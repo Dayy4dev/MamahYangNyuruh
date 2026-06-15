8d948ba5766626cf0e7451ebd75a1ebf4bfa265b
Dayy4dev <dafi.ristiansyah@gmail.com>
2026-06-15 19:45:57 +0700

    feat: add HandCannon, Bullet, and object pooling system

---
+++ Assets/Scripts/HandCannon/HandCannon.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class HandCannon : MonoBehaviour
+{
+    [Header("Weapon Settings")]
+    [SerializeField] private Bullet bulletPrefab;
+    [SerializeField] private Transform firePoint;
+
+    [Header("Pool Configuration")]
+    [SerializeField] private int defaultCapacity = 20;
+    [SerializeField] private int maxPoolSize = 50;
+
+    private IObjectPool<Bullet> bulletPool;
+
+    private void Awake()
+    {
+        bulletPool = new ObjectPool<Bullet>(
+            CreateBullet,         // 1. Callback saat membuat objek baru jika pool kosong
+            OnTakeFromPool,       // 2. Callback saat peluru diambil dari pool
+            OnReturnedToPool,     // 3. Callback saat peluru dikembalikan ke pool
+            OnDestroyPoolObject,  // 4. Callback jika peluru dihancurkan karena melebihi kapasitas maksimal
+            true,                 // Collection check (mencegah error jika rilis objek yang sama dua kali)
+            defaultCapacity,
+            maxPoolSize
+        );
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Bullet bullet = bulletPool.Get();
+
+        bullet.transform.position = firePoint.position;
+        bullet.transform.rotation = firePoint.rotation;
+    }
+
+    // IMPLEMENTASI CALLBACK OBJECT POOL
+
+    private Bullet CreateBullet()
+    {
+        Bullet bulletInstance = Instantiate(bulletPrefab);
+        bulletInstance.SetPool(bulletPool);
+        return bulletInstance;
+    }
+
+    private void OnTakeFromPool(Bullet bullet)
+    {
+        bullet.gameObject.SetActive(true);
+    }
+
+    private void OnReturnedToPool(Bullet bullet)
+    {
+        bullet.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyPoolObject(Bullet bullet)
+    {
+        Destroy(bullet.gameObject);
+    }
+}
