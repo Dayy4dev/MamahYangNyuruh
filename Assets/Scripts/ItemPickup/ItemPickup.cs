b9a1f8083b4c7168820f4c000829efa88e16606d
Dayy4dev <dafi.ristiansyah@gmail.com>
2026-06-21 19:34:28 +0700

    feat: add item pickup animation

--- Assets/Scripts/ItemPickup/ItemPickup.cs
+++ Assets/Scripts/ItemPickup/ItemPickup.cs
@@ -3,8 +3,23 @@ using UnityEngine;
 [AddComponentMenu("Items/Item Pickup")]
 public abstract class ItemPickup : MonoBehaviour
 {
+    [Header("Pickup Settings")]
     [SerializeField] private Collider pickupCollider;
     protected bool alreadyPickedUp = false;
+
+    [Header("Animation Settings")]
+    [SerializeField] private bool enableAnimation = true;
+    
+    [Tooltip("Kecepatan rotasi X, Y, Z")]
+    [SerializeField] private Vector3 rotationSpeed = new Vector3(0f, 90f, 0f);
+    
+    [Tooltip("Tinggi maksimal naik-turun")]
+    [SerializeField] private float floatAmplitude = 0.25f;
+    
+    [Tooltip("Kecepatan naik-turun")]
+    [SerializeField] private float floatSpeed = 2f;
+
+    private float startY; // Menyimpan posisi Y awal
 
     protected virtual void Awake()
     {
@@ -13,8 +28,20 @@ public abstract class ItemPickup : MonoBehaviour
 
         if (pickupCollider != null)
             pickupCollider.isTrigger = true;
+
+        startY = transform.position.y; 
     }
 
+    protected virtual void Update()
+    {
+        if (enableAnimation && !alreadyPickedUp)
+        {
+            transform.Rotate(rotationSpeed * Time.deltaTime);
+
+            float newY = startY + (Mathf.Sin(Time.time * floatSpeed) * floatAmplitude);
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        }
+    }
 
     protected virtual void OnTriggerEnter(Collider other)
     {
@@ -28,6 +55,5 @@ public abstract class ItemPickup : MonoBehaviour
         }
     }
 
- 
     protected abstract void OnPickup(GameObject player);
 }
